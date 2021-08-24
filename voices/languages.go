feff68eecca00b748910c485ba05ec625c300ad6
zivoy <ziv132@gmail.com>
2021-08-24 17:56:02 -0400

    add voices enum

---
+++ voices/languages.go
@@ -0,0 +1,35 @@
+package voices
+
+// full tables
+// http://www.lingoes.net/en/translator/langcode.htm
+// https://www.science.co.il/language/Codes.php
+// https://cloud.google.com/text-to-speech/docs/voices
+
+const (
+	English    = "en" // English US
+	EnglishUK  = "en-UK"
+	Japanese   = "ja"
+	German     = "de"
+	Spanish    = "es"
+	Russian    = "ru"
+	Arabic     = "ar"
+	bengali    = "bn"
+	Czech      = "cs"
+	Danish     = "da"
+	Dutch      = "nl"
+	Finnish    = "fi"
+	Greek      = "el"
+	Hindi      = "hi"
+	Hungarian  = "hu"
+	Indonesian = "id"
+	Khmer      = "km"
+	Latin      = "la"
+	Norwegian  = "nb"
+	Polish     = "pl"
+	Slovak     = "sk"
+	Swedish    = "sv"
+	Thai       = "th"
+	Turkish    = "tr"
+	Ukrainian  = "uk"
+	Vietnamese = "vi"
+)
